c4d48dc5a5f760d494a852058c70a757f8b5f6dd
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-12 22:43:17 +0300

    ticket:1 add mockup and turn it into django tamplate

---
+++ apps/person_info/test/test_index.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+
+class PersonInfoViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get(reverse('index'))
+
+    def test_index(self):
+        """test status code"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_index_html(self):
+        """test template render"""
+        html = '42 Coffee Cups Test Assignment'
+        self.assertTrue(html in self.response.content)
+        self.assertContains(self.response, 'Vinnichuk')
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       url(r'^',
+                           include('apps.person_info.urls')),
+                       url(r'^admin/',
+                           include(admin.site.urls)),
+                       )
+urlpatterns += staticfiles_urlpatterns()
