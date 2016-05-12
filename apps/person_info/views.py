72708fc8d26f2180337ae0c7a983a878aed51f6f
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-12 23:03:30 +0300

    ticket:1 code the view

--- apps/person_info/views.py
+++ apps/person_info/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'person_info/index.html')
+    person = {
+        'first_name': "Vitalii",
+        'last_name': "Vinnichuk",
+        'date_of_birth': "18-03-1993",
+        'bio': "Some bio",
+        'email': "vinnichukvitalii@gmail.com",
+        'jabber': "vinnich@42cc.co",
+        'skype': "vinnichukvitaliy",
+        'other_contacts': "mobile tel: +380973755920"
+    }
+    return render(request, 'person_info/index.html',
+                  {'person': person})
