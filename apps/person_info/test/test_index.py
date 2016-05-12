7714d0846543653ffc4d59b2a5eb1be8fd6419eb
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-12 23:14:55 +0300

    ticket:1 test model

--- apps/person_info/test/test_index.py
+++ apps/person_info/test/test_index.py
@@ -18,3 +18,32 @@ class PersonInfoViewTest(TestCase):
         html = '42 Coffee Cups Test Assignment'
         self.assertTrue(html in self.response.content)
         self.assertContains(self.response, 'Vitalii')
+
+class PersonInfoModelTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_correct_response(self):
+        """test for unicode and object creation"""
+        Person.objects.create(first_name=u'Вася',
+                              last_name=u'Пупкин',
+                              date_of_birth='2000-01-01',
+                              bio=u'Вася молодец',
+                              email='vasyapupkin@pokemon.com',
+                              jabber='vaska007@google.com',
+                              skype='Vasya777',
+                              other_contacts='9379992')
+        new_person = Person.objects.last()
+        self.assertEqual(unicode(new_person.first_name), u'Вася')
+        # Test corect response after object creation
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, 'Vinnichuk')
+
+    def test_db_data_exist(self):
+        """test model data exist objects """
+        Person.objects.all().delete()
+        response = self.client.get(reverse('index'))
+        html_response = 'Error! No person info!'
+        self.assertEqual(Person.objects.all().count(), 0)
+        self.assertIn(html_response, response.content)
