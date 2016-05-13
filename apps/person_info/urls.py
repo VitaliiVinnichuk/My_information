db929dfba9d061d1e08c497313b14701352a7f17
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-13 13:39:53 +0300

    ticket:3 create html mockup and turn it into django template

--- apps/person_info/urls.py
+++ apps/person_info/urls.py
@@ -5,4 +5,7 @@ urlpatterns = patterns('',
                        url(r'^$',
                            'apps.person_info.views.index',
                            name='index'),
+                       url(r'^request_logger/$',
+                           'apps.person_info.views.request_logger',
+                           name='request_logger'),
                        )
--- apps/person_info/views.py
+++ apps/person_info/views.py
@@ -6,3 +6,8 @@ def index(request):
     person = Person.objects.first()
     return render(request, 'person_info/index.html',
                   {'person': person})
+
+
+def request_logger(request):
+    return render(request, 'person_info/request_logger.html',
+                  {'requests': ''})
