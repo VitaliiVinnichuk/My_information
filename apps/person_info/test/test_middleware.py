61dd7b8e8ee5e9d237a8fd2e370c180468d3907d
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-13 14:40:05 +0300

    ticket:3 test view

---
+++ apps/person_info/test/test_middleware.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+
+class MiddleWareLoggerViewTest(TestCase):
+    """Middleware test"""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_request_logger(self):
+        """test status code"""
+        response = self.client.get(reverse('request_logger'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_request_logger_html(self):
+        """test template render"""
+        html = '42 Coffee Cups Test Assignment'
+        response = self.client.get(reverse('request_logger'))
+        self.assertTrue(html in response.content)
+        self.assertTemplateUsed(response, template_name='person_info/request_logger.html')
+        self.assertContains(response, '/request_logger/')
+        self.assertContains(response, 'GET')
+        self.assertContains(response, '2.20.190.43')
