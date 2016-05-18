4a360a170162f870204d29959a724e9ef85cb6be
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2016-05-18 21:43:40 +0300

    ticket:5 create test for view

---
+++ apps/person_info/test/test_edit.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from apps.person_info.forms import PersonForm
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class PersonEditTest(TestCase):
+    def setUp(self):
+        self.request = self.client.get(reverse('edit'))
+        self.person = {'first_name': 'Vitaliy',
+                       'last_name': 'Vinnichuk',
+                       "date_of_birth": "1993-03-18",
+                       'bio': 'some bio',
+                       'email': 'vinnichukvitaliy@gmail.com',
+                       'jabber': 'vinni@42cc.co',
+                       'skype': 'vinnichukvitaliy'
+                       }
+
+    def test_request_logger(self):
+        """test status code"""
+        self.assertEqual(self.request.status_code, 200)
+
+    def test_edit_html(self):
+        """test template render"""
+        html = '42 Coffee Cups Test Assignment'
+        self.assertTrue(html in self.request.content)
+
+    def test_valid_data(self):
+        form = PersonForm(self.person)
+        self.assertTrue(form.is_valid)
+
+    def test_blank_data(self):
+        form = PersonForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['first_name'], ['This field is required.'])
+        self.assertEqual(form.errors['last_name'], ['This field is required.'])
+        self.assertEqual(form.errors['date_of_birth'], ['This field is required.'])
+
+    def test_form_fields(self):
+        # check form fields
+        self.assertIn('name="first_name"', self.request.content)
+        self.assertIn('name="last_name"', self.request.content)
+        self.assertIn('name="date_of_birth"', self.request.content)
+        self.assertIn('name="email"', self.request.content)
+        self.assertIn('name="jabber"', self.request.content)
