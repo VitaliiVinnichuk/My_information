3568a30e1220ee8f55fb3214cf0c1428ded81e37
Vitalii Vinnichuk <vinnichukvitalii@gmail.com>
2017-02-20 19:07:40 +0200

    jenkins time test

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -10,7 +10,6 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # App/Library Paths
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'x=c0_e(onjn^80irdy2c221#)2t^qi&6yrc$31i(&ti*_jf3l8'
